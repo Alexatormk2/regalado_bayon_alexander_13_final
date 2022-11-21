f5930a955b3b8c3474d5ac44e2a951ff9cdb2bb5
Alex <alexander.regalado@ikasle.egibide.org>
2022-11-21 21:53:19 +0100

    continuando diseño de ventanas, falta las de gestion de piezas, proyectos y gestion

---
+++ src/Gestion_Piezas.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Gestion_Piezas {
+    private JButton limpiarButtonProve;
+    private JButton insertarButton;
+    private JButton modificarButtonProve;
+    private JButton eliminarButtonProve;
+    private JTextField textProveedoresDireccion;
+    private JLabel label6;
+    private JTextField textProveedoresApellido;
+    private JLabel label4;
+    private JTextField textProveedoresNombre;
+    private JLabel label3;
+    private JTextField textProveedorCodigo;
+    private JLabel label2;
+    private JLabel label1;
+}
